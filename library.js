04498dac7e2cf88f3f28be6967a1f2be0057cca3
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-09-06 09:00:14 -0400

    Make dummy content an IIfE to execute when the page loads
    This removes the need for a function call at the end of the page. While much of this code could be
    refactored into classes, only one constructor was really called throughout the program. Refactoring
    the add book functions and extending the booklib array to a constructor that extends Book might
    require a massive amount of code surgery that at this moment seems a little excessive outside of
    being syntactically pleasing

--- library.js
+++ library.js
@@ -267,14 +267,14 @@ function resort(direction, flag){
 }
 
 // Dummy content for design of page--DELETE before final push!
-function dummyContent(){
+const dummy=(function dummyContent(){
     const book1= new Book('Hobbit', 'JRR Tolkien', '320', 'not read');
     const book2= new Book('Leviathan Wakes', 'James S.A. Corey', '592', 'read');
     const book3= new Book(`To Kill a Mocking Bird`, 'Harper Lee', '281','read');
     addBook(book1);
     addBook(book2);
     addBook(book3);
-}
+})(0);
 // Legacy Code for future maintence and testing
 /*
 function massiveDummy(){
@@ -301,5 +301,4 @@ function massiveDummy(){
 
 }*/
 
-dummyContent();
 // massiveDummy();
